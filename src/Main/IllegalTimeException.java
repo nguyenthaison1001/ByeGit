dc6d6a613d0714208d038072a2215818481767d9
Nguyen Thai Son <75731710+nguyenthaison1001@users.noreply.github.com>
2020-12-15 14:08:39 +0300

    Lab_04

---
+++ src/Main/IllegalTimeException.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class IllegalTimeException extends Exception{
+    private final String message;
+
+    public IllegalTimeException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/Main/NullDrinkRuntimeException.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class NullDrinkRuntimeException extends RuntimeException{
+    private final String message;
+
+    public NullDrinkRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
